f7566f71b22233568aa0488383b5d43cb9646fb7
asakeev11 <talgat.asakeev@gmail.com>
2023-12-13 01:38:06 +0600

    bugs fixed

--- delete.py
+++ delete.py
@@ -14,3 +14,16 @@ from models import Anime, Genre
 
 # query = Anime.delete().where(Anime.genre == 1)
 # query.execute()
+
+# first_name, last_name, *some, age = ['John', 'Green', 'USA', 'CEO', 'Ferrari', '3 Kids', '45yo']
+# print(first_name, last_name, some, age)
+
+# *_, = range(5)
+# print(_)
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     return n * factorial(n-1)
+#
+#
+# print(factorial(501))
